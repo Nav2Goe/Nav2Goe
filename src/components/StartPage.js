9ff3ec28b0ae29e154acc9ae2a96b4126be2fcab
Morza95 <Buschendorf95@gmx.de>
2021-08-07 14:07:57 +0200

    Build FInal

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -7,9 +7,11 @@ const StartPage = (props) => {
   // ClickHandler
   const [hideStartForm, setStartFormHidden] = useState(false);
   const [callSearchPage, setCallSearchPage] = useState(false);
+  const [hideIntroSentences, setHideIntroSentences] = useState(false);
 
   const continueClickedHandler = () => {
     setCallSearchPage(true);
+    setHideIntroSentences(true);
   };
 
   return (
@@ -20,37 +22,30 @@ const StartPage = (props) => {
           <header className="StartPage-header">
             <h1 className="logo">Nav2Gö</h1>
             <div id="welcomeText">
-              {/* <p>
-                Sie haben sich dazu entschieden, Ihre Freunde in Göttingen zu
-                besuchen. Angekommen am Hauptbahnhof Göttingen, wollen Sie die
-                öffentlichen Verkehrsmittel nutzen, um zu Ihnen weiterfahren.
-                Ihre Freunde haben Ihnen bereits mitgeteilt, dass Sie an der
-                Haltestelle "Treuenhagen" wohnen. Da sie Ihre Freunde nun
-                unglücklicherweise nicht mehr erreichen können, haben Sie sich
-                die lokale Mobilitäts-App „Nav2Gö“ heruntergeladen, um sich vor
-                Ort zurechtzufinden. Nutzen Sie für die Verbindungssuche vom
-                Hauptbahnhof nach Treuenhagen das aktuelle Datum und die
-                aktuelle Uhrzeit.
-              </p> */}
               <p>
+                {hideIntroSentences ? null : (
+                  <>
+                    <br></br>
+                    Wilkommen. <br></br>
+                    Nutzen sie diese App um sich in folgender Situation
+                    weiterzuhelfen:
+                    <br></br>
+                    <br></br>Sie haben sich dazu entschieden, Ihre Freunde in
+                    Göttingen zu besuchen. Nachdem Sie am Hauptbahnhof in Göttingen 
+                    angekommen sind, wollen Sie die öffentlichen Verkehrsmittel 
+                    nutzen, um zu ihren Freunden zu fahren.
+                    <br></br>
+                    <br></br>
+                    Ihre Freunde haben Ihnen bereits mitgeteilt, dass Sie an der
+                    Haltestelle "Treuenhagen" wohnen. Da sie Ihre Freunde nun
+                    unglücklicherweise nicht mehr erreichen können, haben Sie
+                    sich die lokale Mobilitäts-App „Nav2Gö“ heruntergeladen, um
+                    sich vor Ort zurechtzufinden. <br></br> <br></br>
+                  </>
+                )}
+                Nutzen Sie für die Verbindungssuche vom Hauptbahnhof nach
+                Treuenhagen das aktuelle Datum und die aktuelle Uhrzeit.
                 <br></br>
-                Wilkommen. <br></br>
-                Nutzen sie diese App um sich in folgender Situation
-                weiterzuhelfen:
-                <br></br>
-                <br></br>Sie haben sich dazu entschieden, Ihre Freunde in
-                Göttingen zu besuchen. Angekommen am Hauptbahnhof Göttingen,
-                wollen Sie die öffentlichen Verkehrsmittel nutzen, um zu Ihnen
-                weiterfahren.
-                <br></br>
-                <br></br>
-                Ihre Freunde haben Ihnen bereits mitgeteilt, dass Sie an der
-                Haltestelle "Treuenhagen" wohnen. Da sie Ihre Freunde nun
-                unglücklicherweise nicht mehr erreichen können, haben Sie sich
-                die lokale Mobilitäts-App „Nav2Gö“ heruntergeladen, um sich vor
-                Ort zurechtzufinden. <br></br> <br></br>Nutzen Sie für die
-                Verbindungssuche vom Hauptbahnhof nach Treuenhagen das aktuelle
-                Datum und die aktuelle Uhrzeit.<br></br>
                 <br></br>
               </p>
             </div>
@@ -61,7 +56,10 @@ const StartPage = (props) => {
 
       {callSearchPage ? (
         <SearchPage
-          onGoBack={() => setCallSearchPage(false)}
+          onGoBack={() => {
+            setCallSearchPage(false);
+            setHideIntroSentences(false);
+          }}
           onSetStartFormHidden={setStartFormHidden}
         />
       ) : (
@@ -83,3 +81,4 @@ const StartPage = (props) => {
 };
 
 export default StartPage;
+
